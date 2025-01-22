80f5a21ecd4f99df11583cffe76bcc52a8f47048
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-22 19:04:46 +0530

    add basic arithmetic and increment/decrement operations to operators.php

--- operators.php
+++ operators.php
@@ -1,6 +1,24 @@
 <?php
+$x = 10;
+$y = 2;
+$z = $x + $y; // addition
+$z = $x - $y; // subtraction
+$z = $x * $y; // multiplication
+$z = $x / $y; // division
+$z = $x ** $y; // exponential
+$z = $x % $y; // returns remainder of this division
 
+echo $z . "<br>";
 
+// Increment/Decrement operator
+
+$counter = 10;
+
+// $counter = $counter + 1; // cumbersome to write
+// $counter++; // same output and working // increment operator
+$counter--; // decrement operator // same as counter = counter - 1;
+
+echo $counter; 
 ?>
 
 <!DOCTYPE html>
