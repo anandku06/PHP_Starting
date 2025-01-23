0feae2accac48129fe9012170d84371b694b442e
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-24 00:32:27 +0530

    add form for quantity input and calculate total in menuCard.php

--- menuCard.php
+++ menuCard.php
@@ -5,11 +5,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Menu Card</title>
 </head>
-<body>
-    
+<body style="background-color: #212121; color: #fff">
+    <form action="./menuCard.php" method="post">
+        <label for="quantity">Quantity: </label>
+        <input type="text" name="quantity">
+        <input type="submit" name="submit" value="Total">
+    </form>
 </body>
 </html>
 
 <?php
+
+$item = "pizza";
+$price = 5.99;
+$quantity = $_POST["quantity"];
+$total = null;
+
+$total = $quantity * $price;
+
+echo "You have ordered $quantity * $item/s <br>";
+echo "Your total is \${$total}";
 
 ?>
