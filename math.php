8e950bf443bc2a8eda75b83d82bb6065d1997140
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-24 22:23:30 +0530

    add form for user input in math.php with styling

--- math.php
+++ math.php
@@ -4,8 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Math Functions in PHP</title>
+    <style>
+        body{
+            background-color: #212121;
+            color: #fff;
+        }
+    </style>
 </head>
 <body>
-    
+    <form action="./math.php" method="post">
+        <label for="number">X: </label>
+        <input type="text" name="x">
+        <input type="submit" value="Total">
+    </form>
 </body>
 </html>
