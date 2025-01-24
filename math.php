c1310b307fa930ef14888869d2e9a1b4ed29517e
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-24 22:36:42 +0530

    add additional input fields for Y and Z in math.php and implement math functions for calculations

--- math.php
+++ math.php
@@ -15,7 +15,32 @@
     <form action="./math.php" method="post">
         <label for="number">X: </label>
         <input type="text" name="x">
+        <label for="number">Y: </label>
+        <input type="text" name="y">
+        <label for="number">Z: </label>
+        <input type="text" name="z">
         <input type="submit" value="Total">
     </form>
 </body>
 </html>
+
+<?php
+
+$x = $_POST["x"];
+$y = $_POST["y"];
+$z = $_POST["z"];
+echo"".$x."<br>";
+
+// math functions
+$total = abs($x); // returns the absolute value
+$total = round(abs($total)); // rounds up/down the number
+$total = floor(abs($total)); // rounds down the number
+$total = ceil(abs($total)); // rounds up the number
+$total = pow($x, $y); // returns the raised value by the specified number ; exponents
+$total = sqrt($x); // returns the square root of the number
+$maximum = max($x, $y, $z); // returns the maximum number
+$minimum = min($x, $y, $z); // returns the minimum number
+echo"".$total."<br>";
+echo"".$minimum."<br>";
+
+?>
