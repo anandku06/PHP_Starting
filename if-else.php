07bff757c5b30464d6d0ba7314f1af56972e0da2
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-26 23:17:16 +0530

    add weekly pay calculation logic in if-else.php to handle different hour scenarios

--- if-else.php
+++ if-else.php
@@ -28,4 +28,21 @@ else{
     echo "You are too young for this!! <br>";
 }
 
+
+$hours = 90;
+$rate = 15;
+$weekly_pay = null;
+
+if($hours < 0){
+    $weekly_pay = 0;
+}
+elseif($hours <= 40){
+    $weekly_pay = $hours * $rate;
+}
+else{
+    $weekly_pay = ($rate * 40) + (($hours - 40) * ($rate * 1.5));
+}
+
+echo "You made \${$weekly_pay} this week!! <br>";
+
 ?>
