d8fd19c96ae9f2e9d1686ffc1d72708054ef37c9
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-26 22:59:54 +0530

    add radius calculation form and implement circumference and area calculations in radius.php

--- radius.php
+++ radius.php
@@ -5,12 +5,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Radius</title>
 </head>
-<body>
-    
+<body style="background-color: #212121; color: #fff">
+    <form action="./radius.php" method="post">
+        <label for="radius">Radius: </label>
+        <input type="text" name="radius" placeholder="Enter radius">
+
+        <input type="submit" value="Calculate">
+    </form>
 </body>
 </html>
 
 <?php
+$radius = $_POST["radius"];
+$circumference = null;
+$area = null;
 
+$circumference = 2 * pi() * $radius;
+$circumference = round($circumference, 2); // round function takes two args, one the number to be round off and then the precision
 
+$area = pi() * pow($radius, 2);
+$area = round($area, 2);
+
+echo "Circumference: {$circumference}cm <br>";
+echo "Area is: {$area}cm <br>";
 ?>
