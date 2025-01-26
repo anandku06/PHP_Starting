1c09d852e09e9e53f07b1dc63447d83330925c16
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-26 23:06:29 +0530

    add initial if-else structure in if-else.php with age validation logic

---
+++ if-else.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body style="background-color: #212121; color: #fff">
+    
+</body>
+</html>
+
+<?php
+// if statement -> if some condition is true, then we do something
+// else statement -> if the 'if' statement is false, then we do somthing
+
+$age = 12;
+
+if ($age >= 18) {
+    echo "You may enter this site!! <br>";
+}
+else{
+    echo "You are too young for this!! <br>";
+}
+
+?>
