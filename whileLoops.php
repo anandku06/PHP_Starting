cc76a17c3e32c8390718d8c160b639474c44b4f6
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-29 00:32:44 +0530

    add form to control while loop execution in whileLoops.php

--- whileLoops.php
+++ whileLoops.php
@@ -6,12 +6,31 @@
     <title>Document</title>
 </head>
 <body style="background-color: #212121; color: #fff;">
-    
+    <form action="./whileLoops.php" method="post">
+        <input type="submit" name="stop" value="Stop">
+    </form>
 </body>
 </html>
 
 <?php
+// while loop -> do some code infinitely while some condition is true
 
+// $counter = 1;
+// while($counter <= 10){
+//     echo $counter, "<br>";
+//     $counter++;
+// }
 
+$seconds = 0;
+$isRunning = true;
+while($isRunning){
+    if(isset($_POST["stop"])){
+        $isRunning = false;
+    }
+    else{
+        echo $seconds . "<br>";
+        $seconds++;
+    }
+}
 
 ?>
