d1f69923ef8c9168ab41f65d84a48d4ac19ef77e
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-29 22:17:52 +0530

    add examples for modifying and accessing values in associative arrays

--- associativeArray.php
+++ associativeArray.php
@@ -24,4 +24,22 @@ $capitals = array(
 
 echo $capitals["USA"] . "<br>"; // this is how to access the values in associative array, using keys as indices
 
+$capitals["India"] = "Patna"; // this is how to modify the values using the keys as indices
+
+array_pop( $capitals ); // pops the last element from the array
+
+$capKeys = array_keys($capitals); // returns all the keys in this specified array as a new array
+foreach($capKeys as $key){
+    echo "$key <br>";
+}
+
+$capValues = array_values($capitals); // returns all the values in this specified array as a new array
+foreach($capValues as $value){
+    echo "$value <br>";
+}
+
+foreach($capitals as $key => $value){
+    echo "$key => $value <br>";
+} // in the foreach loop giving the key and value as the vars for the pairs in the array to access them
+
 ?>
