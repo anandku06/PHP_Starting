5948373416092ffe691af411dd05abcbc22861d6
Commiter <161126335+anandku06@users.noreply.github.com>
2025-02-01 01:37:04 +0530

    add birthday greeting for multiple names and implement even/odd function

--- BroCode/functions.php
+++ BroCode/functions.php
@@ -23,11 +23,20 @@ we can pass arguments to the function as variable and use them in the code
 */
 function happyBirthday($name){
     echo "Happy Birthday to you!!! <br>";
-    echo "Happy Birthday to $name!!! <br>"; // now we use that given arguments here
+    echo "Happy Birthday to $name!!! <br><br>"; // now we use that given arguments here
 } 
 
 happyBirthday("Keshav"); // this is how you invoke or call the function using the set of parenthesis
 // if the function requires arguments, then in the set of parenthesis we give the parameters that can be use in the place of the arguments
 
+happyBirthday("Ayush");
+happyBirthday("Anand");
+
+function evenOrOdd($num){
+    return $num % 2 == 0 ? "Even!!<br>" : "Odd!!<br>";
+}
+// you can return any value from the function if needed using the return keyword!!
+
+echo evenOrOdd(1);
 
 ?>
