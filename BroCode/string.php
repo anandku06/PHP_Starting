7e2c6ef085d19d3899f8dfc0449be68b05f6c8af
Commiter <161126335+anandku06@users.noreply.github.com>
2025-02-01 17:24:37 +0530

    enhance string manipulation functions in BroCode/string.php with additional operations and phone number formatting

--- BroCode/string.php
+++ BroCode/string.php
@@ -14,12 +14,27 @@
 <?php 
 
 $userName = "Bro Code         ";
+$phoneNumber = "123-456-7890";
 
 $userName = strtolower($userName); // returns the string in lowercase
 $userName = strtoupper($userName); // returns the string in uppercase
 $userName = trim($userName); // trims any leading or former whitespaces
 $userName = str_pad($userName, 20, "$"); // pads the string with another string to the specified length
+$phoneNumber = str_replace("-", "", $phoneNumber); // replace the specified string with the required string
+$userName = strrev($userName); // reverses the string and returns it
+$userName = str_shuffle($userName); // randomly shuffles the string's characters position
+$equals = strcmp($userName, $phoneNumber); // checks whether the two provided strings are same or not
+$length = strlen($userName); // returns the string's length
+
+$posi = strpos($userName,"r"); // returns the position of the specified element from the string
+
+$user = substr($userName, 0, 4); // returns a new string from the specified string according to the starting and ending position given
+
 
 echo $userName . "<br>";
+echo $phoneNumber . "<br>";
+echo $equals . "<br>";
+echo $length . "<br>";
+echo $posi . "<br>";
 
 ?>
