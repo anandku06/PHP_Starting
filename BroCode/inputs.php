ecdbb32e32b5ee553c5c13c9e644d99707f36f48
Commiter <161126335+anandku06@users.noreply.github.com>
2025-02-02 17:51:39 +0530

    validate age and email inputs in inputs.php with appropriate sanitization and error handling

--- BroCode/inputs.php
+++ BroCode/inputs.php
@@ -31,14 +31,31 @@ if(isset($_POST["login"])){
     
     $age = filter_input(INPUT_POST,"age", FILTER_SANITIZE_NUMBER_INT);
     // FILTER_SANITIZE_NUMBER_INT -> filters or sanitizes the input with the characters other than numbers ; you'll only get numbers 
-
-    echo "You are {$age} years old!!<br>";
-
+    
     $email = filter_input(INPUT_POST,"email", FILTER_SANITIZE_EMAIL);
-
+    
     // FILTER_SANITIZE_EMAIL -> filters or sanitize all the extra or not-needed chars from the email entered
+    
+    $age = filter_input(INPUT_POST,"age", FILTER_VALIDATE_INT);
+    // FILTER_VALIDATE_INT -> validates the input variable provided is int or not; if not then returns false (empty string/space)
 
-    echo "Your email is -> {$email}<br>";
+    if(empty($age)){ // if not int, then the filter will return the empty string, making the condition true
+        echo "Input is invalid!!<br>";
+    }
+    else{
+        echo "You are {$age} years old!!<br>";
+    }
+    
+    $email = filter_input(INPUT_POST,"email", FILTER_VALIDATE_EMAIL); 
+
+    // FILTER_VALIDATE_EMAIL -> validates email whether in the correct format or not, if not then return false (empty string/space)
+
+    if(empty($email)){ // if not in the provided email format, then the filter will return the empty string, making the condition true
+        echo "Input is invalid!!<br>";
+    }
+    else{
+        echo "Your email is -> {$email} !!<br>";
+    }
 }
 
 ?>
