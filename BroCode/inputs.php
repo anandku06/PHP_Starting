7adf620facd088c468ed8cde53a08f177a9a7ef5
Commiter <161126335+anandku06@users.noreply.github.com>
2025-02-02 17:40:46 +0530

    add age and email fields to user input form with sanitization

--- BroCode/inputs.php
+++ BroCode/inputs.php
@@ -9,6 +9,10 @@
     <form action="./inputs.php" method="post">
         Username: <br>
         <input type="text" name="username"><br>
+        Age: <br>
+        <input type="text" name="age"><br>
+        Email: <br>
+        <input type="text" name="email"><br>
         <input type="submit" name="login" value="login">
     </form>
 </body>
@@ -18,9 +22,23 @@
 // sanitizing the input field to prevent any mailicious code or script to run or execute
 
 if(isset($_POST["login"])){
-    $username = $_POST["username"];
+    $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS); // used to filter or sanitize any variable's value
+    // here the username taken from the form is sanitized
+    // args are like, first is the input_type(POST), others are the value to be filtered and the criteria of the filteration
+    // (FILTER_SANITIZE_SPECIAL_CHARS) -> filters the variable from any special chars 
 
     echo "Hello {$username}<br>";
+    
+    $age = filter_input(INPUT_POST,"age", FILTER_SANITIZE_NUMBER_INT);
+    // FILTER_SANITIZE_NUMBER_INT -> filters or sanitizes the input with the characters other than numbers ; you'll only get numbers 
+
+    echo "You are {$age} years old!!<br>";
+
+    $email = filter_input(INPUT_POST,"email", FILTER_SANITIZE_EMAIL);
+
+    // FILTER_SANITIZE_EMAIL -> filters or sanitize all the extra or not-needed chars from the email entered
+
+    echo "Your email is -> {$email}<br>";
 }
 
 ?>
