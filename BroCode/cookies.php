5d3efd8f4a4fe64002fb2a7c3ed69bd7b100c439
Commiter <161126335+anandku06@users.noreply.github.com>
2025-02-03 22:08:42 +0530

    update cookies.php to delete dessert cookie and display user preferences

--- BroCode/cookies.php
+++ BroCode/cookies.php
@@ -18,5 +18,18 @@
     // cookies stored as an associative array
     // args are : "key", "value", expireTime in secs, and the file path
     setcookie("favDrink", "Mojito", time() + (86400 * 3), "/"); 
-    setcookie("favDessert", "GulabJamun", time() + (86400 * 4), "/"); 
+    setcookie("favDessert", "GulabJamun", time() - 0, "/"); // to delete the cookie set the time to (- 0)
+
+    foreach($_COOKIE as $key => $value){
+        echo $key ." => ". $value ."<br>";
+    }
+    // $_COOKIE -> a super global variable that has all the cookies used in the website
+
+    if(isset($_COOKIE["favFood"])){
+        echo "Your favourite food is here -> {$_COOKIE["favFood"]}!!<br>";
+    }
+    else{
+        echo "I don't know your food<br>";
+    }
+    // checks whether the cookie of this key is available or not
 ?>
