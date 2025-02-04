b1ff3103e7e65bc10c0bc3f0139199c86046f10c
Commiter <161126335+anandku06@users.noreply.github.com>
2025-02-04 19:48:15 +0530

    add session.php to manage user sessions with basic HTML structure

---
+++ BroCode/session/session.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body style="background-color: #212121; color: #fff">
+    
+</body>
+</html>
+
+<?php 
+    // session -> a super-global variable used to store information on a user to be used across multiple pages.
+    // a user is assigned a session-id ex. login credentials
+
+    
+?>
