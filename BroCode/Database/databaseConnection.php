96ded0f211e498aec0129a3bbd183c53f4864bc5
Commiter <161126335+anandku06@users.noreply.github.com>
2025-02-06 19:20:09 +0530

    add initial database connection file with HTML structure

---
+++ BroCode/Database/databaseConnection.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Database Connection</title>
+</head>
+<body style="background-color: #212121; color: #fff">
+    
+</body>
+</html>
+
+<?php 
+// db connections can be done by two ways:
+// 1. MySQLi extension
+// 2. PDO (PHP Data Objects)
+
+?>
