fcb646a501d29c2111212d02b144e576f7207810
Commiter <161126335+anandku06@users.noreply.github.com>
2025-02-09 19:05:14 +0530

    add variables.php to demonstrate variable usage in PHP with examples

---
+++ LaraCasts/variables.php
@@ -0,0 +1,8 @@
+<?php 
+
+$name = "Lara";
+
+echo "$name , Hello!! <br>"; // to use the var with the string we use doublw quotes
+echo '$name , Hello!! <br>'; // here using the single quotes will consider the var as string 
+
+?>
