a96caa9d3f28516392b556b574123d67a8f483d5
Commiter <161126335+anandku06@users.noreply.github.com>
2025-02-13 13:15:44 +0530

    add about.php and index.php files; update about.view.php greeting message

--- LaraCasts/Pagelinks/about.view.php
+++ LaraCasts/Pagelinks/about.view.php
@@ -128,7 +128,7 @@
         <main>
             <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
                 <!-- Your content -->
-                Hello, Welcome to my Website!!
+                Hello, Welcome to my About Page!!
             </div>
         </main>
     </div>
