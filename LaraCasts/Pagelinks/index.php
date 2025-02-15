5a40fd6f436c0513507a635b97e1f7a24c03e9c8
Commiter <161126335+anandku06@users.noreply.github.com>
2025-02-15 20:05:08 +0530

    move page logic to controllers; delete old page files for about, contact, and index

--- LaraCasts/Pagelinks/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-// take these files as controllers that entertain incoming requests and response of the client-side
-$header = "Home"; // this variable is accessible under the require file as well
-
-require "./functions.php";
-
-require("./views/index.view.php");
