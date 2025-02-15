661a4c5ad52972c1cdcdd262268ffab8182485f6
Commiter <161126335+anandku06@users.noreply.github.com>
2025-02-15 16:05:58 +0530

    add debug output for request URI in about, contact, and index pages

--- LaraCasts/Pagelinks/index.php
+++ LaraCasts/Pagelinks/index.php
@@ -1,5 +1,17 @@
-<?php 
+<?php
 // take these files as controllers that entertain incoming requests and response of the client-side
 $header = "Home"; // this variable is accessible under the require file as well
+
+function dd($value)
+{
+    echo "<pre>";
+    var_dump($value); // dumps info about the provided element
+    echo "</pre>";
+
+
+    die(); // terminates the PHP script
+}
+
+echo ($_SERVER['REQUEST_URI']);
 
 require("./views/index.view.php");
